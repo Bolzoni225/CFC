890b3609392816a38cd0a9d96eb153660ec5ff96
ASOUMAHORO <ASOUMAHORO@ASC-PC110>
2020-12-22 18:51:44 +0000

    ajout de controller écriture fonction d'envoi de mail

--- CFC/App_Data/Models/all.generated.cs
+++ CFC/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "34412342d2904495")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.9")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
---
+++ CFC/Controllers/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace CFC.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+       public ActionResult SendMail(FormCollection Form)
+        {
+            string email = Form["email"].ToString();
+            string phone = Form["phone"].ToString();
+            string objet = Form["objet"].ToString();
+            string message = Form["message"].ToString();
+
+            var client = new SmtpClient() {
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential("user1@test.com", "cedricanselme"),
+                Host="localshot"
+            };
+            //client.Port = 25;
+            //client.Host = "localhost";
+            //client.Credentials = new NetworkCredential("user1@test.com", "cedricanselme");
+            
+            //client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            MailMessage mailMessage = new MailMessage(email,"soumcedric16@gmail.com");
+            mailMessage.Subject = objet;
+            mailMessage.Body = message;
+            try
+            {
+                client.Send(mailMessage);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return null;
+        }
+    }
+}
