67a57f1a6d83720005a84f35c170a092a96795b8
briceyao225 <bricenguessanyao@gmail.com>
2020-12-30 18:19:16 +0000

    mdf 1

--- CFC/Controllers/RDVController.cs
+++ CFC/Controllers/RDVController.cs
@@ -25,45 +25,38 @@ namespace CFC.Controllers
 
 
         [HttpPost]
-       public ActionResult DemandeRdv(FormCollection Data)
+        public ActionResult DemandeRdv(FormCollection Data)
         {
             try
             {
-
+                var nom = Data["Nom"];
+                var prenoms = Data["Prenoms"];
+                var phone = Data["phone"];
+                var mail = Data["email"];
+                var nomets = Data["nomets"];
+                var anneeconstitution = Data["anneeconstitution"];
+                var chiffreAffaire = Data["chiffreAffaire"];
+                var effectif = Data["effectif"];
+                var motif = Data["motif"];
+                var description = Data["description"];
 
                 var NewEvent = new RdvDto()
                 {
-                    AnneeConstitution = RDV.AnneeConstitution,
-                    ChiffreAffaire = RDV.ChiffreAffaire,
-                    DescriptionMotif = RDV.DescriptionMotif,
-                    Email = RDV.Email,
-                    NomDemandeur = RDV.NomDemandeur,
-                    PrenomsDemandeur = RDV.PrenomsDemandeur,
-                    Telephone = RDV.Telephone,
-                    NomEntreprise = RDV.NomEntreprise,
-                    DateRDV = Convert.ToDateTime(RDV.DateRDV),
-                    Fonction = RDV.Fonction,
-                    ObjetRDV = RDV.ObjetRDV
+                    AnneeConstitution = anneeconstitution,
+                    ChiffreAffaire = chiffreAffaire,
+                    DescriptionMotif = description,
+                    Email = mail,
+                    NomDemandeur = nom,
+                    PrenomsDemandeur = prenoms,
+                    Telephone = phone,
+                    NomEntreprise = nomets,
+                    DateRDV = DateTime.Now
 
                 };
-                Sql sql = new Sql("SELECT TOP   1 * FROM TB_RDV ORDER BY ROWIDAUTO DESC");
-                _db.Insert<RdvDto>("TB_RDV", "RowidAuto", NewEvent);
-                var lastRDV = _db.Fetch<RdvDto>(sql).First().RowidAuto;
 
-                foreach (var item in RDV.ListeMotif)
-                {
-                    var motif = new MotifDto()
-                    {
-                        idRDV = lastRDV,
-                        LibelleMotif = item
-                    };
-                    _db.Insert<MotifDto>("TB_MOTIF","ROWIDAUTO",motif);
-                }
-                                                          
-                url = Request.Url.GetLeftPart(UriPartial.Authority);
-                //return Redirect(url + "/success");
-                url = url + "/success";
-                return Json(new { ok = true, chemin = url }, JsonRequestBehavior.AllowGet);
+                _db.Insert<RdvDto>("TB_RDV", "ROWID", NewEvent);
+                var url = Request.Url.GetLeftPart(UriPartial.Authority);
+                return Redirect(url + "/success");
             }
             catch (Exception ex)
             {
@@ -72,5 +65,70 @@ namespace CFC.Controllers
             }
             return View();
         }
+
+        #region Liste Participant ***by brice***
+        [HttpGet]
+        public JsonResult ListeParticipants()
+        {
+            // _db.Fetch<EventDto>(new Sql().Select("*").From("TB_EVENT"));
+            //var liste = await _db.FetchAsync<EventDto>(new Sql().Select("*").From("TB_EVENT"));
+            try
+            {
+                var liste = _db.Fetch<ParticipantDto>(new Sql().Select("*").From("TB_PARTICPANT"));
+                return Json(new { ok = true, data = liste }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+                return Json(new { ok = false, data = "", message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+        #endregion
+
+        #region News Events ***by brice***
+        [HttpGet]
+        public JsonResult NewsEvents()
+        {
+            //var liste = _db.Fetch<EventDto>(new Sql().Select("*").From("TB_EVENT").Where("ESTPUBLIER = @estPublier", "ESTPAYANT = @estPayant", new { estPublier = false, estPayant = true }));
+
+            try
+            {
+                var liste = _db.Fetch<EventDto>(new Sql().Select("*").From("TB_EVENT").Where("ESTPUBLIER = @estPublier", new { estPublier = true }));
+                return Json(new { ok = true, data = liste }, JsonRequestBehavior.AllowGet);
+
+            }
+            catch (Exception ex)
+            {
+                var message = ex.Message;
+                return Json(new { ok = false, data = "", message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+        [HttpPost]//pas soumit au service
+        public JsonResult CreateCurrentEvent(string jsonObject)
+        {
+            try
+            {
+                var currentEvent = JsonConvert.DeserializeObject<CurrentEventModel>(jsonObject);
+                if (currentEvent != null)
+                {
+                    Session["CurrentEvent"] = currentEvent;
+
+                    return Json(new { ok = true, message = "", url = "/FormParticipation" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                    return Json(new { ok = false, message = "Recherche Impossible" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex.Message);
+                return Json(new { ok = false, message = ex.Message });
+            }
+
+        }
+        #endregion
     }
 }
