9363a3b810e9415a093d229e1906266ad234b0e0
ASOUMAHORO <ASOUMAHORO@ASC-PC110>
2020-12-30 14:46:02 +0000

    traitement de la demande de rdv - enregistrement de la demande

---
+++ CFC/Dto/MotifDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CFC.Dto
+{
+    public class MotifDto
+    {
+        public string LibelleMotif { get; set; }
+        public int  idRDV { get; set; }
+    }
+}
--- CFC/Dto/RdvDto.cs
+++ CFC/Dto/RdvDto.cs
@@ -19,6 +19,8 @@ namespace CFC.Dto
         public string MotifDemande { get; set; }
         public string DescriptionMotif { get; set; }
         public DateTime DateRDV {get;set;}
+       public int RowidAuto { get; set; }
+        
 	
     }
 }
