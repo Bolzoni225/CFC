9c5f7c69f74333bffc831fe2e3cc54ae14434fc5
ASOUMAHORO <ASOUMAHORO@ASC-PC110>
2021-01-06 15:35:20 +0000

    commit brice + cedric

---
+++ CFC/Dto/Api.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace CFC.Dto
+{
+    public class Api
+    {
+        public static HttpClient Url { get; set; }
+
+        static Api()
+        {
+            var url = ConfigurationManager.AppSettings["ApiBIZAO"];
+            Url = new HttpClient();
+            Url.BaseAddress = new Uri(url);
+        }
+    }
+}
